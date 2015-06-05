742ca336ddd46f8224bf84f4cabdb76cd31788e1
jmaks1 <my.mailname@mail.ru>
2015-06-05 15:43:17 +0300

    fix url

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -1,7 +1,7 @@
 <?php if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();?>
 
 <!-- main scripts -->
-<script src="script.js"></script>
+<script src="<?php echo SITE_TEMPLATE_PATH?>/script.js"></script>
 
 	</body>
 </html>
