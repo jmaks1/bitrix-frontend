d34880d2446f608d69dc98a345f0433b8c32e1cb
jmaks1 <my.mailname@mail.ru>
2015-08-10 11:20:52 +0300

    add svg & scss func

--- gulpfile.js
+++ gulpfile.js
@@ -7,42 +7,51 @@ var gulp        = require('gulp'),
     uglify      = require('gulp-uglify'),       // Сжимать наш JS
     rigger      = require('gulp-rigger'),       // Позволяет импортировать один файл в другой простой конструкцией
     sass        = require('gulp-sass'),         // для компиляции нашего SCSS кода
-    sourcemaps  = require('gulp-sourcemaps'),   // Для генерации css sourscemaps, которые будут помогать нам при отладке кода
+    sourcemaps  = require('gulp-sourcemaps'),   // Для генерации css sourscemaps, помогает нам при отладке кода
     cssmin      = require('gulp-minify-css'),   // Сжатие CSS кода
     imagemin    = require('gulp-imagemin'),     // Сжатие картинок
     pngquant    = require('imagemin-pngquant'), // Сжатие картинок | работа с PNG
-    spritesmith = require('gulp.spritesmith'),  // Создание png спрайтов
-    plumber     = require('gulp-plumber');      // Ловим ошибки, чтобы не прервался watch
+    plumber     = require('gulp-plumber'),      // Ловим ошибки, чтобы не прервался watch
+    svgSprite   = require('gulp-svg-sprite'),   // Создаем спрайт из svg
+    svgmin      = require('gulp-svgmin'),       // оптимизируем наш svg
+    svg2png     = require('gulp-svg2png'),      // Создадим альтернативный спрайт из svg в png
+    spritesmith = require('gulp.spritesmith');  // Создание png спрайтов
+
 
 // write routs
 var path = {
     build: {
         js:            'local/templates/main/',
-        css:           'local/templates/main/',
+        styles:        'local/templates/main/',
         images:        'local/templates/main/images/',
         fonts:         'local/templates/main/fonts/',
-        fontsBootstrap:'local/templates/main/fonts/bootstrap/'
+        fontBootstrap: 'local/templates/main/fonts/bootstrap/'
     },
     src: {
-        js:            'src/js/script.js',
-        styles:        'src/styles/template_styles.scss',
-        stylesPartials:'src/styles/partials/',
-        spriteTemplate:'src/sass.template.mustache',
-        images:        'src/images/**/*.*',
-        sprite:        'src/sprite/*.*',
-        fonts:         'src/fonts/**/*.*',
-        fontsBootstrap:'bower_components/bootstrap-sass/assets/fonts/bootstrap/*.*'
+        js:                'src/js/*.*',
+        styles:            'src/styles/*.*',
+        stylesPartials:    'src/styles/partials/',
+        images:            'src/images/**/*.*',
+        sprite:            'src/sprite/*.*',
+        spriteTemplate:    'src/sprite-template.scss',
+        spriteSvg:         'src/sprite-svg/*.*',
+        spriteSvgTemplate: 'src/sprite-svg-template.scss',
+        fonts:             'src/fonts/**/*.*',
+        fontBootstrap:     'bower_components/bootstrap-sass/assets/fonts/bootstrap/*.*'
     },
     watch: {
-        js:    'src/js/**/*.js',
-        styles:'src/styles/**/*.scss',
-        images:'src/images/**/*.*',
-        sprite:'src/sprite/*.*',
-        fonts: 'src/fonts/**/*.*'
+        js:        'src/js/**/*.js',
+        styles:    'src/styles/**/*.scss',
+        images:    'src/images/**/*.*',
+        sprite:    'src/sprite/*.*',
+        spriteSvg: 'src/sprite-svg/*.*',
+        fonts:     'src/fonts/**/*.*'
     }
 };
 
 
+
+// javascript
 gulp.task('js:build', function () {
     gulp.src(path.src.js)               // Найдем наш main файл
         .pipe(plumber())
@@ -53,32 +62,7 @@ gulp.task('js:build', function () {
         .pipe(plumber.stop())
         .pipe(gulp.dest(path.build.js)) // Выплюнем готовый файл в build
 });
-
-gulp.task('styles:build', function () {
-    gulp.src(path.src.styles)            // Выберем наш main.scss
-        .pipe(plumber())
-        .pipe(sourcemaps.init())         // То же самое что и с js
-        .pipe(sass())                    // Скомпилируем
-        .pipe(prefixer())                // Добавим вендорные префиксы
-        .pipe(cssmin())                  // Сожмем
-        .pipe(sourcemaps.write())        // Пропишем карты
-        .pipe(plumber.stop())
-        .pipe(gulp.dest(path.build.css)) // И в build
-});
-
-gulp.task('image:build', function () {
-    gulp.src(path.src.images) //Выберем наши картинки
-        .pipe(plumber())
-        .pipe(imagemin({   //Сожмем их
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()],
-            interlaced: true
-        }))
-        .pipe(plumber.stop())
-        .pipe(gulp.dest(path.build.images))
-});
-
+// png sprite
 gulp.task('sprite:build', function() {
     var spriteData =
         gulp.src(path.src.sprite)
@@ -87,7 +71,7 @@ gulp.task('sprite:build', function() {
                 cssName: 'sprite.scss',
                 cssFormat: 'scss',
                 algorithm: 'binary-tree',
-                padding: 20,
+                padding: 5,
                 cssTemplate: path.src.spriteTemplate,
                 cssVarMap: function(sprite) {
                     sprite.name = 's-' + sprite.name
@@ -97,24 +81,88 @@ gulp.task('sprite:build', function() {
     spriteData.img.pipe(gulp.dest(path.build.images));
     spriteData.css.pipe(gulp.dest(path.src.stylesPartials));
 });
-
-gulp.task('icons:build', function() {
-    gulp.src(path.src.fontsBootstrap)
-        .pipe(gulp.dest(path.build.fontsBootstrap));
+// svg sprite
+gulp.task('spriteSvg:build', function () {
+    gulp.src(path.src.spriteSvg)
+        .pipe(plumber())
+        .pipe(svgmin())
+        .pipe(svgSprite({
+            "shape": {
+                "spacing": {
+                    "padding": 5,
+                },
+            },
+            "mode": {
+                "css": {
+                    "dest": "./",
+                    "layout": "diagonal",
+                    "sprite": path.build.images+"sprite-svg.svg",
+                    "bust": false,
+                    "render": {
+                        "scss": {
+                            "dest": path.src.stylesPartials+'sprite-svg.scss',
+                            "template": path.src.spriteSvgTemplate
+                        }
+                    }
+                }
+            }
+        }))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest("./"));
 });
-
+// svg to png sprite | for ie
+gulp.task('svg2png', function() {
+    gulp.src(path.build.images+"sprite-svg.svg")
+        .pipe(plumber())
+        .pipe(svg2png())
+        .pipe(plumber.stop())
+        .pipe(gulp.dest(path.build.images));
+});
+// images
+gulp.task('image:build', function () {
+    gulp.src(path.src.images)
+        .pipe(plumber())
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest(path.build.images))
+});
+// move bootstrap icons(font) to build
+gulp.task('icons:build', function() {
+    gulp.src(path.src.fontBootstrap)
+        .pipe(gulp.dest(path.build.fontBootstrap));
+});
+// move custom fonts to build
 gulp.task('fonts:build', function() {
     gulp.src(path.src.fonts)
         .pipe(gulp.dest(path.build.fonts))
 });
+// styles
+gulp.task('styles:build', function () {
+    gulp.src(path.src.styles)               // Выберем наш main.scss
+        .pipe(plumber())
+        .pipe(sourcemaps.init())            // То же самое что и с js
+        .pipe(sass())                       // Скомпилируем
+        .pipe(prefixer())                   // Добавим вендорные префиксы
+        .pipe(cssmin())                     // Сожмем
+        .pipe(sourcemaps.write())           // Пропишем карты
+        .pipe(plumber.stop())
+        .pipe(gulp.dest(path.build.styles)) // И в build
+});
+
 
 gulp.task('build', [
     'js:build',
     'sprite:build',
+    'spriteSvg:build',
+    'image:build',
     'icons:build',
     'fonts:build',
     'styles:build',
-    'image:build',
 ]);
 
 gulp.task('watch', function(){
@@ -124,14 +172,21 @@ gulp.task('watch', function(){
     watch([path.watch.sprite], function(event, cb) {
         gulp.start('sprite:build');
     });
-    watch([path.watch.styles], function(event, cb) {
-        gulp.start('styles:build');
+    watch([path.watch.spriteSvg], function(event, cb) {
+        gulp.start('spriteSvg:build');
+    });
+    // watch for sprite-svg
+    watch([path.build.images+"sprite-svg.svg"], function(event, cb) {
+        gulp.start('svg2png');
+    });
+    watch([path.watch.images], function(event, cb) {
+        gulp.start('image:build');
     });
     watch([path.watch.fonts], function(event, cb) {
         gulp.start('fonts:build');
     });
-    watch([path.watch.images], function(event, cb) {
-        gulp.start('image:build');
+    watch([path.watch.styles], function(event, cb) {
+        gulp.start('styles:build');
     });
 });
 
